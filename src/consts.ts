9e6d05f7dfd482fd98872c8fa8da0c9524abc709
Sarthak Makhija <sarthak.makhija@gmail.com>
2026-01-20 12:07:40 +0530

    Sarthak | Updates SITE_TITLE

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'Tech Lessons';
+export const SITE_TITLE = 'tech-lessons.in';
 export const SITE_DESCRIPTION = 'Long-form essays on refactoring, storage engines, databases, and system design trade-offs.';
